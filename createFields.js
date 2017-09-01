239f9fd301802a03a00d73b0ca39a0ba31a823b1
Emma D <ecodapo@gmail.com>
2017-08-31 23:21:03 -0400

    added more fields to preview window, experimenting with including map

--- createFields.js
+++ createFields.js
@@ -1,7 +1,32 @@
 function createFields(src){
 	console.log("createFields");
-	var about = '<p>' + $(src).find('.header-4086619068').attr('data-reactid','290').html() + '</p>';
-	return about
+
+	var about = $(src).find('.header-4086619068').attr('data-reactid','290').html() + '</p>';
+	
+	if (about.match(/.*About the Poster.*/)){
+		about = "";
+	} else {
+		about = '<p> Name: ' + about;
+	}
+
+	var owner = '<p> Account Type: ' + $(src).find('.profileItem-1908837923:first').html() + '</p>';
+	var since = '<p>' + $(src).find('.profileItem-1908837923:eq(1)').html() + '</p>';
+	
+	var address = '<p> Location: ' + $(src).find('.address-3119942078').html() + '</p>';
+	var map = '<p>' + $(src).find('.mapImage-1885488482').src() + '</p>';
+
+	//can't get it to work with map
+	console.log(map);
+
+	finalVals = address + map + about + owner + since;
+
+	addFields(finalVals);
+};
+
+function addFields(fieldStr){
+	console.log('add fields');
+	var $newHTML = $(fieldStr);
+	$("#tail").html($newHTML);
 };
 
 function test(){
@@ -14,8 +39,10 @@ function addPreviewContent(link){
 		url: link,
 		type: 'GET',
 		success: function(data){
-			var $newHTML = $(createFields(data));
-			$("#tail").html($newHTML);
+			createFields(data);
+			//$newHTML.map($("#tail").append)
+
+			//$("#tail").html($newHTML);
 //			 $(data).find('.
 //			var about = '<p>' + $(data).find('.header-4086619068').attr('data-reactid','290').html() + '</p>';
 //			var owner = '<p>' + $(data).find('.profileItem-1908837923').html() + '</p>';
