09adc05168464f8e4afcfcbc088bef6e0afc99b7
Emma D <ecodapo@gmail.com>
2017-09-05 19:48:23 -0400

    more styling changes and changed back to pressing shift key to freeze preivew pane

--- createFields.js
+++ createFields.js
@@ -6,44 +6,30 @@ function createFields(src){
 	if (about.match(/.*About the Poster.*/)){
 		about = "";
 	} else {
-		about = '<p> Name: <strong>' + about +'</strong>';
+		about = '<p><strong>' + about +'</strong>';
 	}
 
-	var owner = '<div id="rightleft"><span style="float: left;"><p> Account Type: <strong>' + $(src).find('.profileItem-1908837923:first').html() + '</strong></p>';
+	var owner = '<span style="float: left; padding-left: 10px;">Account Type: <strong>' + $(src).find('.profileItem-1908837923:first').html() + '</strong>';
 	var since = '<p>' + $(src).find('.profileItem-1908837923:eq(1)').html() + '</p></span>';
+	
 	var address =  $(src).find('.address-3119942078').html()
 	var mapAddress = address.replace(/ /g, "+");
 	var location = '<p>' + address + '</p>';
 	var map = '<p> <a href="https://www.google.com/maps/dir//' + mapAddress + '/"  target="_blank"> <img width="400" src="https://maps.googleapis.com/maps/api/staticmap?center=' + mapAddress + '&zoom=13&scale=1&size=400x200&maptype=roadmap&format=png&visual_refresh=true&markers=size:lrg%7Ccolor:0xff99ff%7Clabel:%7C' + mapAddress + '" alt=""></a></p>';
+	
 	var otherAdsLink = $(src).find('.profileLink-4028019214:first').attr('href');
-	var otherAds = '<span style="float: right;"><p><a href=' + otherAdsLink + '  target="_blank">Other Ads</a>';
+	var otherAds = '<span style="float: right; padding-right: 10px;"><a href=' + otherAdsLink + '  target="_blank">Other Ads</a>';
+	
 	var webLink =  $(src).find('.profileLink-4028019214:eq(1)').attr('href');
-	var web = '<p><a href=' + webLink + '  target="_blank">Website</a></p></span></div>';
+	
+	if (typeof webLink === 'undefined'){
+		var web = '</span>';
+	} else {
+		var web = '<p><a href=' + webLink + '  target="_blank">Website</a></p></span>';
+	}
 
 	//finalVals = location + map + about + owner + since;
 	finalVals = [about, location, map, owner, since, otherAds, web];
-	addFields(finalVals);
+	return finalVals;
 };
 
-function addFields(fieldArray){
-	console.log('add fields');
-	var $newHTML = "";
-	fieldArray.map(function(f) {$newHTML += f;});
-	console.log($newHTML);
-	$("#tail").html($newHTML);
-};
-
-function test(){
-	console.log(test);
-};
-
-function addPreviewContent(link){
-	console.log("addPreview");
-	$.ajax({
-		url: link,
-		type: 'GET',
-		success: function(data){
-			createFields(data);
-		}
-	});
-};
