713110fe144fbd87803d83f86738310b03728407
RB <ecodapo@gmail.com>
2017-09-05 18:04:51 -0400

    styling changes and added functionality to interact with preview window while holding z key

--- createFields.js
+++ createFields.js
@@ -6,18 +6,22 @@ function createFields(src){
 	if (about.match(/.*About the Poster.*/)){
 		about = "";
 	} else {
-		about = '<p> Name: ' + about;
+		about = '<p> Name: <strong>' + about +'</strong>';
 	}
 
-	var owner = '<p> Account Type: ' + $(src).find('.profileItem-1908837923:first').html() + '</p>';
-	var since = '<p>' + $(src).find('.profileItem-1908837923:eq(1)').html() + '</p>';
+	var owner = '<div id="rightleft"><span style="float: left;"><p> Account Type: <strong>' + $(src).find('.profileItem-1908837923:first').html() + '</strong></p>';
+	var since = '<p>' + $(src).find('.profileItem-1908837923:eq(1)').html() + '</p></span>';
 	var address =  $(src).find('.address-3119942078').html()
 	var mapAddress = address.replace(/ /g, "+");
-	var location = '<p> Location: ' + address + '</p>';
-	var map = '<p> <img width="400" src="https://maps.googleapis.com/maps/api/staticmap?center=' + mapAddress + '&zoom=13&scale=1&size=400x200&maptype=roadmap&format=png&visual_refresh=true&markers=size:lrg%7Ccolor:0xff99ff%7Clabel:%7C' + mapAddress + '" alt=""> </p>';
+	var location = '<p>' + address + '</p>';
+	var map = '<p> <a href="https://www.google.com/maps/dir//' + mapAddress + '/"  target="_blank"> <img width="400" src="https://maps.googleapis.com/maps/api/staticmap?center=' + mapAddress + '&zoom=13&scale=1&size=400x200&maptype=roadmap&format=png&visual_refresh=true&markers=size:lrg%7Ccolor:0xff99ff%7Clabel:%7C' + mapAddress + '" alt=""></a></p>';
+	var otherAdsLink = $(src).find('.profileLink-4028019214:first').attr('href');
+	var otherAds = '<span style="float: right;"><p><a href=' + otherAdsLink + '  target="_blank">Other Ads</a>';
+	var webLink =  $(src).find('.profileLink-4028019214:eq(1)').attr('href');
+	var web = '<p><a href=' + webLink + '  target="_blank">Website</a></p></span></div>';
 
 	//finalVals = location + map + about + owner + since;
-	finalVals = [location, map, about, owner, since];
+	finalVals = [about, location, map, owner, since, otherAds, web];
 	addFields(finalVals);
 };
 
@@ -25,6 +29,7 @@ function addFields(fieldArray){
 	console.log('add fields');
 	var $newHTML = "";
 	fieldArray.map(function(f) {$newHTML += f;});
+	console.log($newHTML);
 	$("#tail").html($newHTML);
 };
 
