533688bd30eb03225e4f5cbeec96536056a571b0
Emma D <ecodapo@gmail.com>
2017-09-09 15:54:30 -0400

    paused previews now maintain position on scrolling

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ $(document).ready(function(){
 
 	bindPreview();
 
+	$(document).scroll(function() {
+ 		var scrollDiff;
+    	$('.tail').not('.active').each(function(){
+			scrollDiff = $(window).scrollTop() + parseInt($(this).attr('scrollTop'),10);	
+			$(this).toggleClass('transition', true);
+    		$(this).css({
+    			top: scrollDiff
+    		});
+		});
+    });
+
 	$(window).on('resize', bindPreview());
 
 	$(document).keydown(function(e) {
